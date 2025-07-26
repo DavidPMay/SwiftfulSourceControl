93755733b228cd0e5f63f4fe3776c4840c4e8b9f
DavidPMay <maydavidp@icloud.com>
2025-07-26 12:29:51 -0400

    [feature] Add explore view

---
+++ exploreView.swift
@@ -0,0 +1,18 @@
+//
+//  exploreView.swift
+//  SwiftfulSourceControl
+//
+//  Created by David May on 7/26/25.
+//
+
+import SwiftUI
+
+struct exploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    exploreView()
+}
